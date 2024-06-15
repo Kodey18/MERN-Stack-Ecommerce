d9eeb75d405556bb4bd2fed5eec20dc82b549065
Karan <developedbykaran18@gmail.com>
2024-06-15 13:15:30 +0530

    update password

--- backend/controllers/generalControllers/userController.js
+++ backend/controllers/generalControllers/userController.js
@@ -1,40 +1,59 @@
 const async_handler = require("express-async-handler");
 const User = require("../../schema/userModel");
 const errorResponse = require("../../utils/errorResponse");
+// const bcrypt = require("bcryptjs");
+const bcryptjs = require("bcryptjs");
 
-/* 
-Desc:  get user information
-route: /api/v1/user/:userID
+/*
+Desc: get user information
+route: /api/v1/user/
 method: GET
 */
 const getUserInformation = async_handler( async(req, res, next) => {
-    const userID = req.params.userID;
-    console.log({
-        userID : req.params.userID,
-        reqUser : req.user._id,
-    });
-    if(req.user._id.toString() !== userID){
-        return next(new errorResponse("Unauthorized access", 401));
-    } else {
-        console.log("Authorized access.");
-    }
 
     try{
-        const user = await User.findById(userID);
-
-        if(!user){
-            return next(new errorResponse("User not found", 401));
-        }
+        const user = req.user;
 
         return res.status(201).json({
             success: true,
-            user,
+            user
         });
     }catch(err){
         next(err)
     }
 });
 
+/*
+Desc: update User Password
+route: /api/v1/user/
+method: GET
+*/
+const updatePassword = async_handler( async(req, res, next) => {
+    const {oldPassword, newPassword} = req.body;
+
+    try{
+        const user = await User.findById(req.user._id).select("+password");
+
+        const isPasswordMatched = await bcryptjs.compare(oldPassword, user.password);
+
+        if(!isPasswordMatched){
+            return next(new errorResponse("Old password is incorrect", 400));
+        }
+
+        user.password = newPassword;
+
+        await user.save();
+
+        return res.status(200).json({
+            success: true,
+            message: "Password updated successfully",
+        });
+    }catch(err){
+        next(err);
+    }
+});
+
 module.exports = {
     getUserInformation,
+    updatePassword,
 };
--- backend/routes/gen/userGenRoutes.js
+++ backend/routes/gen/userGenRoutes.js
@@ -2,6 +2,7 @@ const Router = require("express").Router();
 const userController = require("../../controllers/generalControllers/userController");
 const verifyToken = require("../../middlewares/verifyToken");
 
-Router.get('/:userID',verifyToken, userController.getUserInformation);
+Router.get('/profile',verifyToken, userController.getUserInformation);
+Router.put('/password/update', verifyToken, userController.updatePassword);
 
 module.exports = Router;
