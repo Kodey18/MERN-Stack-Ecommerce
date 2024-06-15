9224ab6d704d272811eda512629d44fbaa3c7474
Karan <developedbykaran18@gmail.com>
2024-06-15 14:25:10 +0530

    delete user

--- backend/controllers/generalControllers/userController.js
+++ backend/controllers/generalControllers/userController.js
@@ -6,7 +6,7 @@ const bcryptjs = require("bcryptjs");
 
 /*
 Desc: get user information
-route: /api/v1/user/
+route: /api/v1/user/profile
 method: GET
 */
 const getUserInformation = async_handler( async(req, res, next) => {
@@ -53,7 +53,59 @@ const updatePassword = async_handler( async(req, res, next) => {
     }
 });
 
+/*
+Desc: update User Profile
+route: /api/v1/user/profile
+method: PUT
+*/
+const updateUser = async_handler( async(req, res, next) => {
+    try{
+        const newUserData = {
+            name: req.body.name,
+            email: req.body.email
+        }
+    
+        const user = await User.findByIdAndUpdate(req.user._id, newUserData, {
+            new: true,
+            runValidators: true,
+        });
+    
+        return res.status(201).json({
+            success: true,
+            user,
+        });
+    }catch(err){
+        next(err);
+    }
+});
+
+/*
+Desc: Delete User Account
+route: /api/v1/user/delete
+method: DELETE
+*/
+const deleteUserAccount = async_handler(async (req, res, next) => {
+    try {
+        const user = await User.findByIdAndDelete(req.user._id);
+
+        if (!user) {
+            return next(new errorResponse("User not found", 404));
+        }
+
+        res.clearCookie("jwt");
+
+        return res.status(200).json({
+            success: true,
+            message: "User account deleted successfully",
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+
 module.exports = {
     getUserInformation,
     updatePassword,
+    updateUser,
+    deleteUserAccount,
 };
--- backend/routes/gen/userGenRoutes.js
+++ backend/routes/gen/userGenRoutes.js
@@ -3,6 +3,8 @@ const userController = require("../../controllers/generalControllers/userControl
 const verifyToken = require("../../middlewares/verifyToken");
 
 Router.get('/profile',verifyToken, userController.getUserInformation);
+Router.put('/profile', verifyToken, userController.updateUser);
+Router.delete('/delete', verifyToken, userController.deleteUserAccount);
 Router.put('/password/update', verifyToken, userController.updatePassword);
 
 module.exports = Router;
